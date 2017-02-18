31429b48080acb181f4b3997428165289bbf296b
Karl K <Karlk15@ru.is>
2017-02-18 20:36:23 +0000

    in client run cmd ng build then grunt and it should create uglified file and concated

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,17 +1,78 @@
-module.exports = function ( grunt ) {
- grunt.loadNpmTasks('grunt-tslint');
- var taskConfig = {
-   tslint: {
-     src: ['src/**/*.ts'],
-     gruntfile: ['Gruntfile.js'],
-     options: {
-       configuration: "tslint.json",
-       force: true,
-       fix: false
-     },
+// module.exports = function(grunt) {
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+//     tslint: {
+//       src: ['src/**/*.ts'],
+//       gruntfile: ['Gruntfile.js'],
+//       options: {
+//         configuration: "tslint.json",
+//         force: true,
+//         fix: false
+//       }
+//     },
+//     concat: {
+//       options: {
+//         separator: ';'
+//       },
+//       dist: {
+//         src: ['dist/*.js'],
+//         dest: 'dist/concat.js'
+//       }
+//     },
+//     uglify: {
+//       dist: {
+//           files: {
+//               'dist/concat.js': 'src/uglify.js',
+//           }
+//       }
+//     }
+// });
+//
+//   grunt.loadNpmTasks('grunt-contrib-uglify');
+//   grunt.loadNpmTasks('grunt-tslint');
+//   grunt.loadNpmTasks('grunt-contrib-concat');
+//   grunt.registerTask('default', ['tslint', 'concat', 'uglify']);
+//
+// };
+module.exports = function(grunt) {
 
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['dist/*.js','dist/!uglify.js'],
+        dest: 'dist/concat.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        force: true,
+        fix: true
+      },
+      dist: {
+        files: {
+          'dist/uglify.js': ['dist/*.js']
+        }
+      }
+    },
+    tslint: {
+      src: ['src/**/*.ts'],
+      gruntfile: ['Gruntfile.js'],
+      options: {
+        configuration: "tslint.json",
+        force: true,
+        fix: false
+      }
     }
-  };
- grunt.initConfig(taskConfig);
- grunt.registerTask('default', ['tslint']);
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-tslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('test', ['tslint']);
+  grunt.registerTask('default', ['tslint', 'uglify','concat']);
 };
