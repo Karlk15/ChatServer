76ce312ccfdfdf804674dec003f4202335f3368c
Karl K <Karlk15@ru.is>
2017-02-19 18:55:52 +0000

    refactor

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,28 +1,6 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['dist/*.js','dist/!uglify.js'],
-        dest: 'dist/conCatAndUglify/concat.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-        force: true,
-        fix: true
-      },
-      dist: {
-        files: {
-          'dist/conCatAndUglify/uglify.js': ['dist/*.js']
-        }
-      }
-    },
     tslint: {
       src: ['src/**/*.ts'],
       gruntfile: ['Gruntfile.js'],
@@ -33,10 +11,6 @@ module.exports = function(grunt) {
       }
     }
   });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-tslint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('test', ['tslint']);
   grunt.registerTask('default', ['tslint']);
 };
